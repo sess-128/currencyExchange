82fb6608219b4a3dc1e4f34077ef94d3c808c2d2
Андрей Павленко <rrtyuisess@gmail.com>
2024-12-30 23:56:35 +0300

    fix: http 304 error

--- src/main/java/Tester.java
+++
@@ -1,9 +0,0 @@
-import dao.CurrencyDao;
-
-public class Tester {
-    public static void main(String[] args) {
-
-        var currencies = CurrencyDao.getInstance().findAll();
-        System.out.println(currencies);
-    }
-}
